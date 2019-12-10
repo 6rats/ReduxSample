2807fe356130e0b83a747c759060aee59096aad3
Takuro Murata <include.takuro@gmail.com>
2019-12-11 08:58:31 +0900

    Initial Commit

---
+++ GistEditor/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  GistEditor
+//
+//  Created by murata on 2019/12/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
